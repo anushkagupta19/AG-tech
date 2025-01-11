ba577b73e1900858b07c7d964ae29ef01cd1ed10
Anushka Gupta <anushkamor19@gmail.com>
2025-01-12 00:38:33 +0530

    Create Construct k palindrome strings.

---
+++ 1400 leetcode.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(s.size()<k) return false;
+        int count[26]={0};
+        
+        for(char c:s)  count[c-'a']++;      
+        int outOccuringChar=0;
+        
+        for(int i=0;i<26;i++)   outOccuringChar+= count[i]%2;
+        
+        return k >= outOccuringChar;  
+}
+};
+    
